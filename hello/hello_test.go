07d4dbc54914d443a87cd4b76e445911d402c001
杨恺 <yangkai02@qutoutiao.net>
2019-12-25 19:07:19 +0800

    test

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,16 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got '%q' want '%q'", got, want)
+	}
+}
+
+func BenchmarkHello(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Hello("world")
 	}
 }
