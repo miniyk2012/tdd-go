a09e61f2eb58cb7302e0f884dc0a429d066f7a94
杨恺 <yangkai02@qutoutiao.net>
2019-12-25 19:44:54 +0800

    helper

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,12 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got '%q' want '%q'", got, want)
+	assertCorrectMessage := func(t *testing.T, got string, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("当输入为空时, 默认提供world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func BenchmarkHello(b *testing.B) {
