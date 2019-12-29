7853218944ec567e26b690ea43e55abe40f84d84
Thomas Young <yk_ecust_2007@163.com>
2019-12-29 10:43:55 +0800

    struct

--- structs/shapes.go
+++ structs/shapes.go
@@ -6,6 +6,10 @@ type Rectangle struct {
 	Height float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Height + rectangle.Width)
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,11 +13,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12, 6}
-	got := Area(rectangle)
-	want := 72.0
+	t.Run("rectangle Area", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	
+	t.Run("circle Area", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.16
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	
 }
