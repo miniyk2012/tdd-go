40b7e02cea480bd589f51c6752cdef37603bf86e
Thomas Young <yk_ecust_2007@163.com>
2019-12-29 10:53:00 +0800

    interface

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,21 +1,34 @@
 package structs
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
 
 type Rectangle struct {
 	Width float64
 	Height float64
 }
 
+func (rectangle Rectangle) Area() float64 {
+	return rectangle.Width * rectangle.Height
+}
+
 type Circle struct {
 	Radius float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
+// 在 Go 语言中 interface resolution 是隐式的。如果传入的类型匹配接口需要的，则编译正确
+func (circle Circle) Area() float64 {
+	return math.Pi * circle.Radius * circle.Radius
+}
+
+func (rectangle Rectangle) Perimeter() float64 {
 	return 2 * (rectangle.Height + rectangle.Width)
 }
 
 
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
-}
+
+
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10, 20}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 60.0
 
 	if got != want {
@@ -13,24 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangle Area", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		got := rectangle.Area()
-		want := 72.0
-
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
+	}
+
+	t.Run("rectangle Area", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72)
 	})
 	
 	t.Run("circle Area", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.16
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, circle, 314.159265358973)
 	})
 	
 }
