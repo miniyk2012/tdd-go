e67b3bbce7770ce32921b52d8aa4e4d6b1440b10
Thomas Young <yk_ecust_2007@163.com>
2019-12-29 10:13:18 +0800

    append

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -33,4 +33,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %v want %v", got, want)
 		}
 	})
+
+	t.Run("空切片传入", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want :=[]int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
