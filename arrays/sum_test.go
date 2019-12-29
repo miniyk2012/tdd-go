b5ab9aeeabeeb24eba5ef560f2a9503811c99394
Thomas Young <yk_ecust_2007@163.com>
2019-12-29 10:23:12 +0800

    checkSum

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -6,45 +6,41 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	checkSum := func(t *testing.T, got []int, want []int) {
+	checkSum := func(t *testing.T, got, want []int) {
+		// 在 Go 中不能对切片使用等号运算符, 必须迭代每个元素
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
 	}
 
-	t.Run("测试切片", func(t *testing.T) {
+	t.Run("测试Sum", func(t *testing.T) {
 		numbers := []int{1, 2, 3} // slice
 		got := Sum(numbers)
 		want := 6
 
-		checkSum(t, got, want)
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
 	})
 
 	t.Run("测试smallSum", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9}, []int{-10, 5, -6}, []int{})
 		want := []int{3, 9, -11, 0}
 
-		// 在 Go 中不能对切片使用等号运算符, 必须迭代每个元素
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSum(t, got, want)
 	})
 
 	t.Run("测试SumAllTails", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2, 5}, []int{0, 9, 12, 15})
 		want := []int{7, 36}
 
-		checkSum(got, want, t)
+		checkSum(t, got, want)
 	})
 
 	t.Run("空切片传入", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5}, []int{2})
 		want := []int{0, 9, 0}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSum(t, got, want)
 	})
 }
-
-
