8216ed4f72d4ee8d8858be3c7a41572fe4fa2ec5
Thomas Young <yk_ecust_2007@163.com>
2019-12-29 10:18:37 +0800

    empty slice

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -16,8 +16,8 @@ func TestSum(t *testing.T) {
 		}
 	})
 	t.Run("测试smallSum", func(t *testing.T) {
-		got := SumAll([]int{1, 2}, []int{0, 9}, []int{-10, 5, -6})
-		want := []int{3, 9, -11}
+		got := SumAll([]int{1, 2}, []int{0, 9}, []int{-10, 5, -6}, []int{})
+		want := []int{3, 9, -11, 0}
 
 		// 在 Go 中不能对切片使用等号运算符, 必须迭代每个元素
 		if !reflect.DeepEqual(got, want) {
@@ -35,8 +35,8 @@ func TestSum(t *testing.T) {
 	})
 
 	t.Run("空切片传入", func(t *testing.T) {
-		got := SumAllTails([]int{}, []int{3, 4, 5})
-		want :=[]int{0, 9}
+		got := SumAllTails([]int{}, []int{3, 4, 5}, []int{2})
+		want := []int{0, 9, 0}
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
