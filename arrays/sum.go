547410f25a43505de8334c9b510b297fb35fbbe3
Thomas Young <yk_ecust_2007@163.com>
2019-12-29 09:59:24 +0800

    make

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,19 @@
 package arrays
 
-func Sum(numbers [5]int) (sum int) {
+import "fmt"
+
+func Sum(numbers []int) (sum int) {
 	for _, num := range numbers {
 		sum += num
 	}
 	return
 }
 
+func SumAll(numbersToSum ...[]int) (sums []int)  {
+	sums = make([]int, len(numbersToSum))
+	fmt.Print(numbersToSum)
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,14 +1,27 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [...]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
+	t.Run("测试切片", func(t *testing.T) {
+		numbers := []int{1, 2, 3} // slice
+		got := Sum(numbers)
+		want := 6
 
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+	t.Run("测试smallSum", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		// 在 Go 中不能对切片使用等号运算符, 必须迭代每个元素
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
-
