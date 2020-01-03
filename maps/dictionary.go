723d2c8f2134c1994b49cf16cb79e6a171e9ac03
杨恺 <yangkai02@qutoutiao.net>
2020-01-03 17:18:20 +0800

    dict Add wordExist

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,9 +4,11 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ( ErrNotFound = errors.New("could not find the word you were looking for")
-    ErrWordExists = errors.New("word has existed")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("word has existed")
 )
+
 func (dic Dictionary) Search(word string) (string, error) {
 	// 通过 map[key] 的方式从 map 中获取值
 	value, ok := dic[word]
@@ -18,10 +20,14 @@ func (dic Dictionary) Search(word string) (string, error) {
 
 func (dic Dictionary) Add(key string, value string) error {
 	// dic是个引用, 因此不需要传递指针. Map 作为引用类型是非常好的，因为无论 map 有多大，都只会有一个副本。
-	_, ok := dic[key]
-	if ok {
+	_, err := dic.Search(key)
+	switch err {
+	case ErrNotFound:
+		dic[key] = value
+	case nil:
 		return ErrWordExists
+	default:
+		return err
 	}
-	dic[key] = value
 	return nil
 }
