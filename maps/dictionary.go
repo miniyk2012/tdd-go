9ed5bcb5b5652d02f7627e062c6fe15fff711818
杨恺 <yangkai02@qutoutiao.net>
2020-01-03 14:04:29 +0800

    assertDefinition

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExists = errors.New("word has existed")
 func (dic Dictionary) Search(word string) (string, error) {
 	// 通过 map[key] 的方式从 map 中获取值
 	value, ok := dic[word]
@@ -14,7 +15,12 @@ func (dic Dictionary) Search(word string) (string, error) {
 	return "", ErrNotFound
 }
 
-func (dic Dictionary) Add(key string, value string) {
+func (dic Dictionary) Add(key string, value string) error {
 	// dic是个引用, 因此不需要传递指针. Map 作为引用类型是非常好的，因为无论 map 有多大，都只会有一个副本。
+	_, ok := dic[key]
+	if ok {
+		return ErrWordExists
+	}
 	dic[key] = value
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -26,17 +26,36 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
+		err := dictionary.Add(word, definition)
 
+		assertError(t, err, nil)
+
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word string, definition string) {
+	got, err := dictionary.Search(word)
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
-	if want != got {
-		t.Errorf("got '%s' want '%s'", got, want)
+	if got != definition {
+		t.Errorf("got '%s' want '%s'", got, definition)
 	}
 }
 
