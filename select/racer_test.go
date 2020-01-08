3522a7eb0a3fb2b64c0b0d92b153a762c0b0810e
杨恺 <yangkai02@qutoutiao.net>
2020-01-08 14:05:32 +0800

    new server

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,7 +1,10 @@
 package racer
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestRace(t *testing.T) {
@@ -15,3 +18,25 @@ func TestRace(t *testing.T) {
 		t.Errorf("got '%s', want '%s'", got, want)
 	}
 }
+
+func TestRacer(t *testing.T) {
+	slowServer :=httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer :=httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL  // http://127.0.0.1:52446
+	fastURL := fastServer.URL  // http://127.0.0.1:52447
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if want != got {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
+
