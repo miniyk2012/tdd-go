f6cfaf20f3fd73529ee9a42eb22a0a582d4d9e46
杨恺 <yangkai02@qutoutiao.net>
2020-01-13 14:00:44 +0800

    slice part1

--- dive_slice/slce_tools.go
+++ dive_slice/slce_tools.go
@@ -1,20 +1,23 @@
 package dive_slice
 
 import (
-	"fmt"
 	"regexp"
 )
 
 var digitRegexp = regexp.MustCompile("[0-9]+")
+
 func AppendByte(slice []byte, data ...byte) []byte {
 	return nil
 }
 
-
-
-func Append(s []int, v int) {
-	a := append(s, v)
-	fmt.Printf("a.cap=%d, a.len=%d, a=%v\n", cap(a), len(a), a)
-	fmt.Printf("s.cap=%d, s.len=%d, s=%v\n", cap(s), len(s), s)
-	fmt.Printf("%p\n", &s)
+// 扩Cap, 则需要新建一个slice, 把旧的复制过去
+func DoubleCap(s []string) []string {
+	t := make([]string, len(s), (cap(s)*2)+1)
+	//for i, v := range s {
+	//	t[i] = v
+	//}
+	copy(s, t)  // 拷贝len(s)长度的数据
+	return t
 }
+
+
--- dive_slice/slce_tools_test.go
+++ dive_slice/slce_tools_test.go
@@ -2,12 +2,62 @@ package dive_slice
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
+func AppendDemo(s []int, v int) {
+	// s是个拷贝的slice, 但s底层的数组是同一个
+	a := append(s, v)
+	s = append(s, 2*v)
+	s[2] = 100
+	fmt.Printf("in function, a.cap=%d, a.len=%d, a=%v\n", cap(a), len(a), a)
+	fmt.Printf("in function, s.cap=%d, s.len=%d, s=%v\n", cap(s), len(s), s)
+	fmt.Printf("%p\n", &s)
+}
+
 func TestAppend(t *testing.T) {
-	s := make([]int, 0, 5)
-	Append(s, 10)
+	s := make([]int, 3, 5)
+	AppendDemo(s, 10)
 	fmt.Printf("s.cap=%d, s.len=%d, s=%v\n", cap(s), len(s), s)
 	fmt.Printf("%p", &s)
+}
+
+func TestEqual(t *testing.T) {
+	var s []byte
+	fmt.Printf("%v\n", s)
+	s = make([]byte, 4, 5)
+	if !reflect.DeepEqual(s, []byte{0, 0, 0, 0}) {
+		t.Error("xx")
+	}
+
+	x := [3]string{"Лайка", "Белка", "Стрелка"}
+	x_s := x[:] // a slice referencing the storage of x
+	x_s[1] = "12"
+	fmt.Println(x_s)
+	fmt.Println(x)
+}
+
+func TestCap(t *testing.T) {
+	s := make([]byte, 5, 5)
+	s = s[2:4]
+	if len(s) != 2 || cap(s) != 3 {
+		t.Error("xx")
+	}
+	s = s[:cap(s)]
+	if len(s) != 3 || cap(s) != 3 {
+		t.Error("xx")
+	}
+}
+
+func TestGrowingSlice(t *testing.T) {
+	s := []string{"a", "b"}
+	new_s := DoubleCap(s)
+	if len(new_s) != len(s) || cap(new_s) != 5 {
+		t.Error("xx")
+	}
+	if !reflect.DeepEqual(s, new_s) {
+		t.Error("xx")
+	}
+
 }
