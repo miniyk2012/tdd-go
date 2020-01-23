de978d5a099149ed5021141be296b95d430e8bf7
杨恺 <yangkai02@qutoutiao.net>
2020-01-23 16:57:39 +0800

    roman part3

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -8,14 +8,27 @@ import (
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
 		Description string
-		Arabic int
-		Want string
+		Arabic      int
+		Want        string
 	}{
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to II", 2, "II"},
 		{"2 gets converted to III", 3, "III"},
 		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
 		{"5 gets converted to V", 5, "V"},
+		{"6 gets converted to VI", 6, "VI"},
+		{"7 gets converted to VII", 7, "VII"},
+		{"8 gets converted to VIII", 8, "VIII"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
+		{"14 gets converted to XIV", 14, "XIV"},
+		{"18 gets converted to XVIII", 18, "XVIII"},
+		{"20 gets converted to XX", 20, "XX"},
+		{"39 gets converted to XXXIX", 39, "XXXIX"},
+		{"40 gets converted to XL", 40, "XL"},
+		{"47 gets converted to XLVII", 47, "XLVII"},
+		{"49 gets converted to XLIX", 49, "XLIX"},
+		{"50 gets converted to L", 50, "L"},
 
 	}
 	for _, test := range cases {
@@ -29,23 +42,31 @@ func TestRomanNumerals(t *testing.T) {
 		})
 	}
 
+}
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
 
+var RomanNumerals = []RomanNumeral{
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
 }
 
 func ConvertToRoman(arabic int) string {
 	var ret strings.Builder
 
-	for i:=arabic; i>0; i-- {
-		if i == 5 {
-			ret.WriteString("V")
-			break
+	for _, number := range RomanNumerals {
+		for arabic >= number.Value {
+			ret.WriteString(number.Symbol)
+			arabic -= number.Value
 		}
-		if i == 4 {
-			ret.WriteString("IV")
-			break
-		}
-		ret.WriteString("I")
 	}
 	return ret.String()
 }
